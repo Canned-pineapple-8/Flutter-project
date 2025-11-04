d1762e69ea6d81250e0d0bb02e3ab5cce6018a07
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2025-11-04 21:06:41 +0300

    Register HTTP-client

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,9 +1,17 @@
 import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
 import 'package:talker_flutter/talker_flutter.dart';
+import '../data/dio/set_up.dart';
 
 final getIt = GetIt.instance;
 final talker = TalkerFlutter.init();
 
+final dio = Dio();
+
 Future<void> setupLocator() async {
   getIt.registerSingleton(talker);
+
+  setUpDio();
+
+  getIt.registerSingleton<Dio>(dio);
 }
