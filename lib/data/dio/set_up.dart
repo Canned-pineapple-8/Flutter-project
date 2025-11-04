c1602a464e549e70289fc61bde240ff701bee19c
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2025-11-04 21:07:07 +0300

    Register HTTP-client

---
+++ lib/data/dio/set_up.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
+import '../../di/di.dart';
+
+void setUpDio() {
+  dio.options.baseUrl = 'https://dummyjson.com'; // базовый URL API
+  dio.options.connectTimeout = const Duration(seconds: 5);
+  dio.options.receiveTimeout = const Duration(seconds: 5);
+
+  dio.interceptors.addAll([
+    TalkerDioLogger(
+      talker: talker,
+      settings: TalkerDioLoggerSettings(
+        printRequestData: true,
+        printRequestHeaders: true,
+      ),
+    ),
+  ]);
+}
