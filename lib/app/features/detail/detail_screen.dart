f5023555fc015462013ed8101c0d794142fc7e64
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2025-11-05 14:27:40 +0300

    Add new info on detail screen

--- lib/app/features/detail/detail_screen.dart
+++ lib/app/features/detail/detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../di/di.dart';
 import 'bloc/detail_bloc.dart';
+import '../../extensions/widget_extensions.dart';
 
 class DetailScreen extends StatefulWidget {
   final int id;
@@ -51,17 +52,39 @@ class _DetailScreenState extends State<DetailScreen> {
                     width: 500,
                     fit: BoxFit.cover,
                   ),
-                  const SizedBox(height: 16),
+                  16.ph,
                   Text(
                     content.title,
                     style: Theme.of(context).textTheme.headlineLarge,
                     textAlign: TextAlign.center,
                   ),
-                  const SizedBox(height: 16),
+                  16.ph,
                   Text(
                     content.description,
                     style: Theme.of(context).textTheme.bodyMedium,
                     textAlign: TextAlign.center,
+                  ),
+                  16.ph,
+                  Text(
+                    "Цена: \$${content.price}",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  16.ph,
+                  Text(
+                    "Скидка: ${content.discountPercentage}%",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  16.ph,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Рейтинг: ${content.rating}",
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      6.pw,
+                      const Icon(Icons.star, color: Colors.amber),
+                    ],
                   ),
                 ],
               ),
--- lib/domain/repositories/content/model/content.dart
+++ lib/domain/repositories/content/model/content.dart
@@ -9,12 +9,19 @@ class Content {
   final String description;
   @JsonKey(name: 'thumbnail')
   final String image;
+  final double price;
+  // @JsonKey(name: 'discountPercentage')
+  final double discountPercentage;
+  final double rating;
 
   Content({
     required this.id,
     required this.title,
     required this.description,
     required this.image,
+    required this.price,
+    required this.discountPercentage,
+    required this.rating,
   });
 
   factory Content.fromJson(Map<String, dynamic> json) =>
