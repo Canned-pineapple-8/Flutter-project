1e68d07d361c1ccc39c7efc79415d57bb2069841
Canned-pineapple-8 <163037624+Canned-pineapple-8@users.noreply.github.com>
2025-12-16 18:35:32 +0300

    Revert "Add FireBase initialization"

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'di/di.dart';
 import 'package:talker/talker.dart';
 import 'lab_project.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +10,6 @@ void main() async {
   FlutterError.onError = (details) {
     return getIt<Talker>().handle(details.exception, details.stack);
   };
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const AppName());
 }
